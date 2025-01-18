ab659c68cc981d35fed29b57b47ba5d12a6c2d76
yash27007 <work.yashwantharavind007@gmail.com>
2025-01-18 14:16:15 +0530

    Revert "Merge branch 'main' of https://github.com/yash27007/maduraiacm"
    This reverts commit 97d220ba068f22ef54f9d43673c2aca3457aacdb, reversing
    changes made to cb00c29cc79acedf68a73c7cd3a81d4b42d9149c.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Use the React plugin
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/maduraiacm/',
-  plugins: [react()], // Use the React plugin here
-});
+  plugins: [react()],
+})
